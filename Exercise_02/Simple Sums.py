72e0f5cae8a79e14c8cba253acc489bc5c7e40cc
Princess <pjamila30@gmail.com>
2024-10-22 12:42:42 +0400

    Update Simple Sums.py

--- Exercise_02/Simple Sums.py
+++ Exercise_02/Simple Sums.py
@@ -1,6 +1,7 @@
 '''( 1 ) Exercise 2 - Simple Sums'''
 # Princess Jamila Dinglasan
 
+'''( 2 ) Variables'''
 # Declaring a variable and initializing it with 8
 bread = 8
 
@@ -10,5 +11,6 @@ butter = 10
 # Declaring a variable that stores the sum of both numbers
 toast = (bread + butter)
 
+'''( 3 ) Print'''
 # Printing the value of the sum
 print(toast)
