37a96e52549123eef1172afff2e0c348eb2b8483
Princess <pjamila30@gmail.com>
2024-11-07 11:05:38 +0400

    Update Coding is cool.py

--- Exercise_01/Coding is cool.py
+++ Exercise_01/Coding is cool.py
@@ -2,12 +2,9 @@
 # Princess Jamila Dinglasan
 
 '''( 2 ) Variables'''
-# We create variables by assigning a name to them
-# Since we will be using "+", we will manually input the spacing
 bread = "Coding "
 bun = "is"
 butter = " cool!"
 
 '''( 3 ) Print'''
-# We then use the print command with the variables created
 print(bread + bun + butter)
