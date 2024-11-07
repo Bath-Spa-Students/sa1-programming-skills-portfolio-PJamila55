d9d287e5183727f07cb58435a386010a6e3fdab0
Princess <pjamila30@gmail.com>
2024-11-07 23:11:31 +0400

    Create Some Counting.py

---
+++ Exercise_07/Some Counting.py
@@ -0,0 +1,37 @@
+'''( 1 ) Exercise 7 - Counting Loops'''
+# Princess Jamila Dinglasan
+
+'''( 2 ) Loops'''
+print("\n")
+# Count up from 0 to 50
+print("Loop 1 : Counting from 0 to 50:")
+for num in range(51):
+    print(num, end=" ")
+print("\n")
+
+# Count down from 50 to 0
+print("Loop 2 : Counting from 50 to 0:")
+for num in range(50, -1, -1):
+    print(num, end=" ")
+print("\n")
+
+# Count up from 30 to 50
+print("Loop 3 : Counting from 30 to 50:")
+for num in range(30, 51):
+    print(num, end=" ")
+print("\n")
+
+# Count down from 50 to 10, decreasing by 2
+print("Loop 4 : Counting down from 50 to 10 by 2s:")
+for num in range(50, 9, -2):
+    print(num, end=" ")
+print("\n")
+
+# Count up from 100 to 200, increasing by 5
+print("Loop 5 : Counting from 100 to 200 by 5s:")
+for num in range(100, 201, 5):
+    print(num, end=" ")
+print("\n")
+
+print("Thank you! (:")
+print("\n")
