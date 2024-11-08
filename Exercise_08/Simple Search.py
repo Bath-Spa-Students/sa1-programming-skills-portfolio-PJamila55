b3fe345491b65d3b01a9f188e9dea8f31ff6c2ea
Princess <pjamila30@gmail.com>
2024-11-08 18:10:52 +0400

    Update Simple Search.py

--- Exercise_08/Simple Search.py
+++ Exercise_08/Simple Search.py
@@ -3,12 +3,18 @@
 
 '''( 2 ) Creating Dictionary'''
 people = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
-search_term = input("Who are you looking for? ").lower()
 
-'''( 3 ) If - Else Statement'''
+'''( 3 ) Asking the User'''
+initial_search = input("Would you like to search for Sam? (yes/no): ").strip().lower()
+
+if initial_search == "yes":
+    print("Searching...")
+    search_term = "sam"
+else:
+    search_term = input("Who would you like to look for? ").strip().lower()
+
+'''( 4 ) If - Else Statement'''
 if search_term in [name.lower() for name in people]:
     print(f"{search_term.capitalize()} is in the list!")
-    print("\n")
 else:
     print(f"{search_term.capitalize()} is not on the list.")
-    print("\n")
