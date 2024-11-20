5d20827cf504dfdb31a998110e26e02238ca5bb4
Princess <pjamila30@gmail.com>
2024-11-20 23:00:29 +0400

    Create 5 Control Structures.py

---
+++ Z Bonus_Exercises/5 Control Structures.py
@@ -0,0 +1,8 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("The player just earned 5 points!")
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("This message won't print since the condition isn't met.")
