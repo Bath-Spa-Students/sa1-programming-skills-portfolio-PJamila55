25d95d3bc85d771f1632b36348c41afc9a13b19d
Princess <pjamila30@gmail.com>
2024-11-20 23:34:37 +0400

    Create 6 Dictionaries.py

---
+++ Z Bonus_Exercises/6 Dictionaries.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'Dylan',
+    'last_name': 'Dinglasan',
+    'age': 24,
+    'city': 'Sharjah'
+}
+
+print(f"First Name: {person['first_name']}")
+print(f"Last Name: {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
