d7fc9e569ca78b3910750156b3df8429d1f17705
Princess <pjamila30@gmail.com>
2024-11-20 23:40:21 +0400

    Create 7 Pizza Toppings.py

---
+++ Z Bonus_Exercises/7 Pizza Toppings.py
@@ -0,0 +1,7 @@
+while True:
+    topping = input("Enter a pizza topping (or type 'quit' to stop): ").strip()
+    if topping.lower() == 'quit':
+        print("Thank you! Your pizza will be prepared.")
+        break
+    else:
+        print(f"I'll add {topping} to your pizza.")
