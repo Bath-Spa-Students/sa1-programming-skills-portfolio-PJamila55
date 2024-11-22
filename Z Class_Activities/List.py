6e2a3f49369a0b3023114b632fb4e500904e794f
Princess <pjamila30@gmail.com>
2024-11-22 15:12:32 +0400

    Create List.py

---
+++ Z Class_Activities/List.py
@@ -0,0 +1,11 @@
+'''Creating a List'''
+
+pastry = list(("bread", "another bread", "more pastry idk"))
+print(pastry[2])
+
+fish = ["3 tilapia", "oarfish", "32", "clownfish"]
+print (fish[0])
+
+rocks = ["rock1", "rock2", "rock3", "0", "THE rock"]
+print (fish[3])
+print (rocks[2])
